ed0083ba6fd8be5a2377d9897af2852b804dd5ce
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-12-19 08:31:00 +0900

    Add: middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 import { AhiModule } from './modules/ahi.module';
 import { AppService } from './services/app.service';
 
@@ -8,4 +9,8 @@ import { AppService } from './services/app.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('ahi');
+  }
+}

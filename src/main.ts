89d9c3b57b951c38927294b29fccd1fa9def69c1
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-12-19 10:30:30 +0900

    Add: exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(8080);
 }
 bootstrap();
